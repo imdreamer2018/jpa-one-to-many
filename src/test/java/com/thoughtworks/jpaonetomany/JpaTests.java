e0c3e3387b7571c809145097cdef21fb9ba36e5f
yangqian <qian.yang2@thoughtworks.com>
2021-11-16 16:18:00 +0800

    [yangqian] #N/A feat: Rename test

--- src/test/java/com/thoughtworks/jpaonetomany/JpaTests.java
+++ src/test/java/com/thoughtworks/jpaonetomany/JpaTests.java
@@ -3,15 +3,17 @@ package com.thoughtworks.jpaonetomany;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
 @SpringBootTest(classes = JpaOneToManyApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Rollback
 class JpaTests {
 
     @Autowired
     private UserRepository userRepository;
 
     @Test
-    void createUser() {
+    void createContactForUser() {
         UserBuilder.withDefault().persist();
         User user = userRepository.findById(1).get();
         user.getContacts().add(ContactBuilder.withDefault().build());
